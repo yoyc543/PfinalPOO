a9faa35e5977640aa143f49c9ed7a028e8f82d34
EduardoC06 <105380241+EduardoC06@users.noreply.github.com>
2023-09-10 20:44:28 -0500

    Se creo Consultorio

---
+++ CitasReserva/src/citasreserva/Consultorio.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package citasreserva;
+
+/**
+ *
+ * @author 51973
+ */
+public class Consultorio {
+    private int idConsultorio;
+    private int idMedico;
+    private String nombreConsultorio;
+
+
+    public int getIdConsultorio() {
+        return idConsultorio;
+    }
+
+
+    public void setIdConsultorio(int idConsultorio) {
+        this.idConsultorio = idConsultorio;
+    }
+
+
+    public int getIdMedico() {
+        return idMedico;
+    }
+
+
+    public void setIdMedico(int idMedico) {
+        this.idMedico = idMedico;
+    }
+
+
+    public String getNombreConsultorio() {
+        return nombreConsultorio;
+    }
+
+
+    public void setNombreConsultorio(String nombreConsultorio) {
+        this.nombreConsultorio = nombreConsultorio;
+    }
+    
+    
+}
